a609d5a735be5ecb04f37fb4aff1e56f95aab22b
aneez004 <aneez004@gmail.com>
2021-12-01 18:29:42 +0530

    delete ouput.tf

--- output.tf
+++
@@ -1,7 +0,0 @@
-output "voting_ip" {
-  value = kubernetes_service.vote-svc.status.0.load_balancer.0.ingress.0.ip
-}
-
-output "result_ip" {
-  value = kubernetes_service.result-svc.status.0.load_balancer.0.ingress.0.ip
-}

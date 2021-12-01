6b25a658b4cb39133fb920a0c3ef24916eb90db9
aneez004 <aneez004@gmail.com>
2021-12-01 18:13:28 +0530

    added zero

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "voting_ip" {
-  value = kubernetes_service.vote-svc.status.0.load_balancer.0.ingress.ip
+  value = kubernetes_service.vote-svc.status.0.load_balancer.0.ingress.0.ip
 }
 
 output "result_ip" {
-  value = kubernetes_service.result-svc.status.0.load_balancer.0.ingress.ip
+  value = kubernetes_service.result-svc.status.0.load_balancer.0.ingress.0.ip
 }
